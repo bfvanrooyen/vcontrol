53ab23172f454252a4ce763466bfc955b8c73a62
bfvanrooyen <bf.v.rooyen@barendict.com>
2016-01-06 19:44:47 +0200

    Got optional positional commands working.

--- cli_commands/list_command.py
+++ cli_commands/list_command.py
@@ -4,8 +4,8 @@ from subprocess import call
 
 def parse_arguments():
     parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
-    parser.add_argument("ls", action="store_true", help="List available Virtual Machines")
+    parser.add_argument("list", help="List available Virtual Machines", nargs="?", default="")
     args = parser.parse_args()
 
-    if args.ls:
+    if args.list == "list":
        call(["vboxmanage", "list", "vms"]) 
