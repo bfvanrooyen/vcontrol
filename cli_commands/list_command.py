1af17378af18d432847a9d723c79d50f3c6e7f84
bfvanrooyen <bf.v.rooyen@barendict.com>
2016-01-07 11:30:07 +0200

    Using subcommands

--- cli_commands/list_command.py
+++ cli_commands/list_command.py
@@ -1,11 +1,9 @@
 import argparse
-
 from subprocess import call
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
-    parser.add_argument("list", help="List available Virtual Machines", nargs="?", default="")
-    args = parser.parse_args()
-
-    if args.list == "list":
-       call(["vboxmanage", "list", "vms"]) 
+def parse_arguments(subparsers):
+    list_parser = subparsers.add_parser('list', help='List available Virtual Machines')
+    list_parser.set_defaults(func=handle_command)
+    
+def handle_command(args):
+    call(['vboxmanage', 'list', 'vms'])
