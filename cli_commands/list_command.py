42a6f9a850b413d450722d4c9274c4b458932107
bfvanrooyen <bf.v.rooyen@barendict.com>
2016-01-07 12:38:15 +0200

    Introducing classes

--- cli_commands/list_command.py
+++ cli_commands/list_command.py
@@ -1,9 +1,11 @@
 import argparse
 from subprocess import call
+import command_base
 
-def parse_arguments(subparsers):
-    list_parser = subparsers.add_parser('list', help='List available Virtual Machines')
-    list_parser.set_defaults(func=handle_command)
+class ListCommand(BaseCommand):
+  def parse_arguments(subparsers):
+      list_parser = subparsers.add_parser('list', help='List available Virtual Machines')
+      list_parser.set_defaults(func=handle_command)
     
-def handle_command(args):
-    call(['vboxmanage', 'list', 'vms'])
+  def handle_command(args):
+      call(['vboxmanage', 'list', 'vms'])
---
+++ command_base.py
@@ -0,0 +1,4 @@
+class BaseCommand
+  def parse_arguments(self, subparsers):
+  def handle_command(self, args):
+
